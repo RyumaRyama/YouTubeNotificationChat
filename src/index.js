2f6ae807a45fd614457e46321499977094378833
RyumaRyama <ryumaryama@gmail.com>
2023-02-10 23:30:32 +0900

    動作制御が完成

--- src/index.js
+++ src/index.js
@@ -7,17 +7,51 @@ const ws = new ReconnectingWebSocket(
   }
 );
 
+// メッセージ管理用のキュー
+const chatItemResponseBodyQueue = [];
+
+// ここに処理を書くと、チャット取得をきっかけとして動く
 ws.onmessage = (chatItemResponse) => {
+  // スパチャなどをはじく
+  // 通常チャットのみを処理
   const chatItemResponseBody = JSON.parse(chatItemResponse.data);
-  console.log(chatItemResponseBody);
-  const chatType = chatItemResponseBody.type;
   const chatItem = chatItemResponseBody.body;
 
   if (!chatItem?.message) return;
 
+  chatItemResponseBodyQueue.push(chatItemResponseBody);
+  // createNotification(chatItem);
+};
+
+// 1秒に1回メッセージを確認する
+setInterval(() => {
+  const newChatItemResponseBody = chatItemResponseBodyQueue.shift();
+  if (newChatItemResponseBody === undefined) {
+    return;
+  }
+  createNotification(newChatItemResponseBody);
+}, 1200);
+
+
+let notification;
+
+// 通知風チャットを作成する
+const createNotification = (chatItemResponseBody) => {
+  const chatItem = chatItemResponseBody.body;
+
+  // 事前に通知があればそれは消す
+  if (notification !== undefined) {
+    notification.remove();
+  }
+
   // 通知全体
-  const notification = document.createElement('div');
+  notification = document.createElement('div');
   notification.classList.add('notification');
+
+  // 通知音
+  const audio = document.createElement('audio');
+  audio.src = './teroren.mp3';
+  audio.autoplay = true;
 
   // サムネイル
   const thumbnail = document.createElement('img');
@@ -76,6 +110,5 @@ const printNotification = (notification) => {
   // 5秒後に消す
   setTimeout(() => {
     notification.remove();
-    console.log('delete');
-  }, 5000);
+  }, 3000);
 };
